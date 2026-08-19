929fe714055ca3cb993c7205365adab02fbaa6e4
shekhardixit-droid <shekhar.dixit@datacircles.in>
2026-08-19 18:29:51 +0530

    Save local changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -20,6 +20,7 @@ import Casesstudy from "./components/project/Casestudy";
 import Manifesto from "./components/project/Manifesto";
 import Webdesign2 from "./components/project/Webdesign2";
 import ThreeServices from "./pages/ThreeServices";
+ import ThinkUFind from "./pages/ThinkUFind";
 
 
 function App() {
@@ -36,7 +37,9 @@ function App() {
           <Route
           path="/disclosures"
           element={<Disclosures />}
+         
         />
+        <Route path="/think-u-find" element={<ThinkUFind />} />
         {/* Service Pages */}
         <Route path="/brand-identity" element={<BrandIdentity />} />
         <Route path="/web-design" element={<WebDev />} />
