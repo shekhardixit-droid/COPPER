f9c8c191b1075b6b4dbdd6eb9379eba20d10cb42
shekhardixit-droid <shekhar.dixit@datacircles.in>
2026-09-11 12:13:27 +0530

    use onboarding@resend.dev until domain verified

--- backend/src/utils/sendNotification.js
+++ backend/src/utils/sendNotification.js
@@ -1,8 +1,8 @@
 const { Resend } = require("resend");
 
 const TEAM_EMAIL    = "shekhar.dixit@datacircles.in";
-const FROM_TEAM     = "Copper Studio <shekhar.dixit@datacircles.in>";
-const FROM_USER     = "Copper Studio <shekhar.dixit@datacircles.in>";
+const FROM_TEAM     = "Copper Studio <onboarding@resend.dev>";
+const FROM_USER     = "Copper Studio <onboarding@resend.dev>";
 
 /**
  * Send 2 emails after a successful MongoDB insert:
